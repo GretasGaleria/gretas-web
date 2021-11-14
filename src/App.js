38cb19f082326d94de961f9f78a281ab75c20506
Marcelo <marcelocsc@hotmail.com>
2021-11-14 19:43:06 -0300

    Added react unity webgl package

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Unity, { UnityContext } from "react-unity-webgl";
+
+const unityContext = new UnityContext({
+  productName: "Galeria Gretas",
+  companyName: "Gretas",
+  loaderUrl: "Build/GretasWeb2.loader.js",
+  dataUrl: "Build/GretasWeb2.data",
+  frameworkUrl: "Build/GretasWeb2.framework.js",
+  codeUrl: "Build/GretasWeb2.wasm",
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Unity unityContext={unityContext} />;
     </div>
   );
 }
