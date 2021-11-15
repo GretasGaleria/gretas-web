6e4a27f15f043f3b60fa97e3839ce2a46d603acc
Marcelo <marcelocsc@hotmail.com>
2021-11-14 22:16:14 -0300

    Testing compressed build

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ const unityContext = new UnityContext({
   productName: "Galeria Gretas",
   companyName: "Gretas",
   loaderUrl: "buildUnity/GretasWeb2.loader.js",
-  dataUrl: "buildUnity/GretasWeb2.data",
-  frameworkUrl: "buildUnity/GretasWeb2.framework.js",
-  codeUrl: "buildUnity/GretasWeb2.wasm",
+  dataUrl: "buildUnity/GretasWeb2.data.br",
+  frameworkUrl: "buildUnity/GretasWeb2.framework.js.br",
+  codeUrl: "buildUnity/GretasWeb2.wasm.br",
 });
 
 function App() {
