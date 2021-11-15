35bc207acde0fb1e44b0359861e931b089da6582
Marcelo <marcelocsc@hotmail.com>
2021-11-14 22:38:25 -0300

    Minimal canvas

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import Unity, { UnityContext } from "react-unity-webgl";
 const unityContext = new UnityContext({
   productName: "Galeria Gretas",
   companyName: "Gretas",
-  loaderUrl: "buildUnity/GretasWeb2.loader.js",
-  dataUrl: "buildUnity/GretasWeb2.data.br",
-  frameworkUrl: "buildUnity/GretasWeb2.framework.js.br",
-  codeUrl: "buildUnity/GretasWeb2.wasm.br",
+  loaderUrl: "Build/GretasWeb2.loader.js",
+  dataUrl: "Build/GretasWeb2.data.br",
+  frameworkUrl: "Build/GretasWeb2.framework.js.br",
+  codeUrl: "Build/GretasWeb2.wasm.br",
 });
 
 function App() {
