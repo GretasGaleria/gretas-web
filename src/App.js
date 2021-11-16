9ef435a237043d4234026e98f0aea071c2cc0317
Marcelo <marcelocsc@hotmail.com>
2021-11-15 23:34:26 -0300

    New build

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import Unity, { UnityContext } from "react-unity-webgl";
 const unityContext = new UnityContext({
   productName: "Galeria Gretas",
   companyName: "Gretas",
-  loaderUrl: "Build/gretas-test.loader.js",
-  dataUrl: "Build/gretas-test.data.br",
-  frameworkUrl: "Build/gretas-test.framework.js.br",
-  codeUrl: "Build/gretas-test.wasm.br",
+  loaderUrl: "Build/gretas-build.loader.js",
+  dataUrl: "Build/gretas-build.data.br",
+  frameworkUrl: "Build/gretas-build.framework.js.br",
+  codeUrl: "Build/gretas-build.wasm.br",
 });
 
 function App() {
