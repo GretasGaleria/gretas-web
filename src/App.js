fa9a5a8ebbc74e12c12031c8c792eb09f129aeec
Marcelo <marcelocsc@hotmail.com>
2021-11-18 16:12:02 -0300

    New build with 2nd room

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import Unity, { UnityContext } from "react-unity-webgl";
 const unityContext = new UnityContext({
   productName: "Galeria Gretas",
   companyName: "Gretas",
-  loaderUrl: "Build/gretas-0.2.loader.js",
-  dataUrl: "Build/gretas-0.2.data",
-  frameworkUrl: "Build/gretas-0.2.framework.js",
-  codeUrl: "Build/gretas-0.2.wasm",
+  loaderUrl: "Build/gretas-0.3.loader.js",
+  dataUrl: "Build/gretas-0.3.data.br",
+  frameworkUrl: "Build/gretas-0.3.framework.js.br",
+  codeUrl: "Build/gretas-0.3.wasm.br",
 });
 
 function App() {
