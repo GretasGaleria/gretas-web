240c8a08535e9fe8abe623304c6ed05d5813c83a
Marcelo <marcelocsc@hotmail.com>
2021-11-24 20:07:09 -0300

    .

--- src/components/Galeria.js
+++ src/components/Galeria.js
@@ -2,10 +2,10 @@ import React from "react";
 import Unity, { UnityContext } from "react-unity-webgl";
 
 const unityContext = new UnityContext({
-  loaderUrl: "../../public/build/gretas-0.3.loader.js",
-  dataUrl: "../../public/build/gretas-0.3.data.br",
-  frameworkUrl: "../../public/build/gretas-0.3.framework.js.br",
-  codeUrl: "../../public/build/gretas-0.3.wasm.br",
+  loaderUrl: "public/build/gretas-0.3.loader.js",
+  dataUrl: "public/build/gretas-0.3.data.br",
+  frameworkUrl: "public/build/gretas-0.3.framework.js.br",
+  codeUrl: "public/build/gretas-0.3.wasm.br",
 });
 
 const Galeria = () => {
