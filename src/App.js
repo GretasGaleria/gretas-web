581cffc78f3460cc440a3e3ec2a8b60e77a5307a
Marcelo <marcelocsc@hotmail.com>
2021-11-24 21:21:28 -0300

    .

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Galeria from "./components/Galeria";
-import GaleriaTeste from "./components/Galeria";
+import GaleriaTeste from "./components/GaleriaTeste";
 
 const App = () => {
   return (
