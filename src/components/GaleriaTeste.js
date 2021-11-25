f9c4f9cfeedb62526778ae4ba5e5452408d0c1a4
Marcelo <marcelocsc@hotmail.com>
2021-11-24 21:14:56 -0300

    test

--- src/components/GaleriaTeste.js
+++ src/components/GaleriaTeste.js
@@ -1,17 +1,24 @@
 import React from "react";
-import Unity, { UnityContext } from "react-unity-webgl";
+import Unity, { UnityContext, SetFullScreen } from "react-unity-webgl";
 
 const unityContext = new UnityContext({
-  loaderUrl: "build/gretas-0.3.loader.js",
-  dataUrl: "build/gretas-0.3.data.br",
-  frameworkUrl: "build/gretas-0.3.framework.js.br",
-  codeUrl: "build/gretas-0.3.wasm.br",
+  loaderUrl:
+    "https://gretasgaleria.blob.core.windows.net/data/build/gretas-0.3.loader.js",
+  dataUrl:
+    "https://gretasgaleria.blob.core.windows.net/data/build/gretas-0.3.data.br",
+  frameworkUrl:
+    "https://gretasgaleria.blob.core.windows.net/data/build/gretas-0.3.framework.js.br",
+  codeUrl:
+    "https://gretasgaleria.blob.core.windows.net/data/build/gretas-0.3.wasm.br",
 });
 
 const Galeria = () => {
   return (
     <div>
-      <Unity unityContext={unityContext} />
+      <Unity
+        unityContext={unityContext}
+        style={{ width: "960px", height: "600px" }}
+      />
     </div>
   );
 };
