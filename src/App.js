b4778cf7ac89e3403c16bc63b66e42cff4f7bdbc
Marcelo <marcelocsc@hotmail.com>
2021-12-01 22:27:55 -0300

    removed test gallery

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Galeria from "./components/Galeria";
-import GaleriaTeste from "./components/GaleriaTeste";
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/galeria" element={<Galeria />} />
-        <Route path="/teste/galeria2" element={<GaleriaTeste />} />
         <Route path="/" element={<Home />} />
       </Routes>
     </Router>
